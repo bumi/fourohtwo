f4d09b57bfbeea2796568228f9661e2abe45793a
iemwill <willie@laubenheimer.eu>
2018-11-21 22:45:02 +0100

    Add notification for Paymentrequest 402

--- plugLN/scripts/background/listen402.js
+++ plugLN/scripts/background/listen402.js
@@ -1,9 +1,19 @@
 var fourohtwo = 402;
+var title = "Paymentrequest 402";
+
+function paymentNot() {
+	browser.notifications.create({
+  		"type": "basic",
+  		"iconUrl": browser.extension.getURL("icons/BitcoinLN.jpg"),
+  		"title": title,
+  		"message": "Do you want to pay ...? -> Open the Plugin ;)",
+	});
+}
 
 function paycheck(request) {
 	if (request.statusCode == fourohtwo) {
 		console.log("Request Value: " + request.statusCode + " URL: " + request.url);
-		browser.browserAction.openPopup();
+		paymentNot();
 	}
 }
 
@@ -12,3 +22,8 @@ browser.webRequest.onHeadersReceived.addListener(
   {urls:  ["<all_urls>"]}, /*Here we can change the urls we want to check.*/
   ["blocking"]
 );
+
+browser.notifications.onClicked.addListener(function handleClick(notificationID) {
+	console.log("NotificationID: " + notificationID)
+	browser.browserAction.openPopup("default_popup");
+});
